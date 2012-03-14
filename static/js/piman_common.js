ebd4f2baf4ec3d64dd7e530c31feea438de860ad
root <root@piman.brc.iop.kcl.ac.uk>
2012-03-14 19:04:13 +0000

    bit of js organisation

---
+++ static/js/piman_common.js
@@ -0,0 +1,13 @@
+/* piman_edit("entity_type", "id") 
+ *  This basically does an Ajax request to grab the CRUD form at /entity_type/id
+ *  and shows it in a pop up - unsure how to implement this at the moment. Should
+ *  it be an iframe so the back button works? Can you do that and toggle its
+ *  visibility so it shows as an overlay without messing up the rest of the page 
+ *  format?
+ *  
+*/
+var piman_edit = function(entity, id) {
+  //alert(entity + ' ' + id);
+   
+}
+
