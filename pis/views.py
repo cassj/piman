072f7da076083dd122c1ebad70e2577f8b00d9a9
root <root@piman.brc.iop.kcl.ac.uk>
2012-04-19 21:24:12 +0100

    working js forms

--- pis/views.py
+++ pis/views.py
@@ -37,20 +37,110 @@ import re
 #  return response
 
 
+def get_projects(pi):
+  try:
+    projects = pi.projects.all()
+  except ObjectDoesNotExist:
+    projects = []
+  return projects
+
+def get_teaching_commitments(pi):
+  try:
+    teach = pi.teaching_commitments.all()
+  except ObjectDoesNotExist:
+    teach = []
+  return teach  
+
+def get_applications(pi):
+  try:
+    applications =  pi.applications.all()
+  except ObjectDoesNotExist:
+    return [],[]
+
+  submitted_applications = applications.filter(submitted_grant__decision_date__gte=datetime.now())
+  awarded_applications = applications.filter(submitted_grant__award__isnull=False)
+  return submitted_applications, awarded_applications
+
+def get_publications(pi):
+  try:
+    publications = pi.author.publications.all()
+  except ObjectDoesNotExist:
+    publications = []
+  return publications
+
+@ajax_login_required
+def students_block(request):
+  try:
+    pi = request.user.pi
+    projects = get_projects(pi)
+    t = loader.get_template('pis/students.html')
+    c = RequestContext(request, {'pi'       : pi,
+                                 'projects' : projects }  )
+    html = t.render(c)
+  except ObjectDoesNotExist:
+    # this should fire a js warning that user is not a PI
+    html = ""
+  
+  response =  HttpResponse(json.dumps({'html' : html}), mimetype=u'application/json')
+  return response
+
+@ajax_login_required
+def teaching_block(request):
+  try:
+    pi = request.user.pi
+    teaching_commitments = get_teaching_commitments(pi)
+    t = loader.get_template('pis/teaching.html')
+    c = RequestContext(request, {'pi'       : pi,
+                                 'teaching_commitments' : teaching_commitments }  )
+    html = t.render(c)
+  except ObjectDoesNotExist:
+    html = ""
+
+  response = HttpResponse(json.dumps({'html' : html}), mimetype=u'application/json')
+  return response
+
+@ajax_login_required
+def grants_block(request):
+  try:
+    pi = request.user.pi
+    submitted_applications, awarded_applications = get_applications(pi)
+    t = loader.get_template('pis/grants.html')
+    c = RequestContext(request, {'pi'                      : pi,
+                                 'submitted_applications'  : submitted_applications,
+                                 'awarded_applications'    : awarded_applications })    
+    html = t.render(c)
+  except ObjectDoesNotExist:
+    html =  ""
+
+  response = HttpResponse(json.dumps({'html' : html}), mimetype=u'application/json')
+  return response
+
+
+@ajax_login_required
+def publications_block(request):
+  try: 
+    pi = request.user.pi
+    publications = get_publications(pi)
+    t = loader.get_template('pis/publications.html')
+    c = RequestContext(request, {'pi'           : pi, 
+                                 'publications' : publications })
+    html = t.render(c)
+  except ObjectDoesNotExist:
+    html = ""
+
+  response = HttpResponse(json.dumps({'html' : html}), mimetype=u'application/json')
+  return response
+
+
 @login_required
 def profile(request):
 
   try:
     pi = request.user.pi
-    projects = pi.projects.all()
-    teaching_commitments = pi.teaching_commitments.all()   
-    applications = pi.applications.all()
-    submitted_applications = applications.filter(submitted_grant__decision_date__gte=datetime.now())
-    awarded_applications = applications.filter(submitted_grant__award__isnull=False) 
-    try:
-      publications = pi.author.publications.all()
-    except ObjectDoesNotExist:
-      publications = []
+    projects = get_projects(pi)
+    teaching_commitments = get_teaching_commitments(pi)
+    submitted_applications, awarded_applications = get_applications(pi)
+    publications = get_publications(pi)
 
     t = loader.get_template('pis/pi_home.html')
     c = RequestContext(request, {'pi'                     : pi,
@@ -84,4 +174,16 @@ def manager_home(request, manager_id):
    t = loader.get_template('pis/manager_home.html')
    c = RequestContext(request, {'manager': manager})
    return HttpResponse(t.render(c))
+
+
+
+def register(request):
+
+  t = loader.get_template('registration/registration_form.html')
+  c = RequestContext(request,{})
+  html = t.render(c)
+  response =  HttpResponse(json.dumps({'html' : html}), mimetype=u'application/json')
+  return(response)
+
+
 
--- static/js/piman_common.js
+++ static/js/piman_common.js
@@ -1,12 +1,7 @@
-/* piman_edit("entity_type", "id") 
- *  This basically does an Ajax request to grab the CRUD form at /entity_type/id
- *  and shows it in a pop up - unsure how to implement this at the moment. Should
- *  it be an iframe so the back button works? Can you do that and toggle its
- *  visibility so it shows as an overlay without messing up the rest of the page 
- *  format?
- *  
-*/
-
+/* Obviously the auth is actually done on the server and unauth 
+ * users won't get any data back but this checks the returned json 
+ * for a not_authenticated entry so we can handle it gracefully
+ */
 function check_authentication( parsed_json ){
   if (parsed_json.not_authenticated){
     return false;
@@ -14,58 +9,51 @@ function check_authentication( parsed_json ){
   return true;
 }
 
-function send_notification(message){
-  $("notifications").html("<p>" + message + "</p>");
-  setTimeout( function(){
-                       $('notifications').show('slow').delay(5000).hide('slow');
-                        },
-              0
-  );
+
+/* Post a message in the notifications div
+ * Will fade onclick or in 5sec
+ */
+function piman_send_message(msg,error){
+ msg = '<span>' + msg + '</span>';
+
+ $(window).scrollTop($('body').position().top)
+
+ $("#notifications_content").html(msg)
+   .addClass(error ? 'error' : 'success');
+ 
+ $("#notifications")
+   .toggle()
+   .delay(5000).fadeOut()
+   .click(function(){$(this).stop(true,true).fadeOut()});
+ 
 }
 
-var piman_edit = function(entity_type, id) {
-
-  url =  '/' + entity_type + '/' + id + '/' + 'edit';
-
-  alert(url);
-
-   var on_success = function(data, textStatus, XMLHttpRequest){
-                       try{
+/* Expects the url to return a json construct like {html: ... }
+ * the html will be loaded into the #overlay div and it will be 
+ * made visible.
+ */
+function piman_overlay(url){
+  var on_success = function(data, textStatus, XMLHttpRequest){
+                      try{
                          if(data){
                            if(check_authentication(data)){
-                             $("#overlay").append(data.html);
-                             //toggle vis?
+                             $("#overlay").html(data.html).show();
                            }else{
                              alert("not logged in");
                            }
-                         }                      
+                         }
                        }catch(error){
                          $("#header").append("<h1>Error parsing JSON</h1>");
                        }
-                    }
-   var on_error = function(XMLHttpRequest, textStatus, errorThrown){
-                     $("#header").append("<h1 class='error'>Error: " + errorThrown + "</h1>");
+
+  }
+
+  var on_error = function(XMLHttpRequest, textStatus, errorThrown){
+                    piman_send_message(errorThrown, true);
+                    // $("#header").append("<h1 class='error'>Error: " + errorThrown + "</h1>");
 
                   }
-   /*
-   function(data, textStatus, XMLHttpRequest){
-              alert("aaaagh")
-               if(data){
-                 alert("here")
-                 if(check_authentication(data)){
-                   $("#overlay").html(data.html);
-
-                 }else{
-                   // this should just redirect the top page to the login page.
-                   alert("not logged in");
-                 }
-               }
-             },
-   */
-
-  // submit a post request to the url with no data
-  // you should get the empty form back, unless you get json
-  $.ajax({
+   $.ajax({
     url: url,
     data: {},
   })
@@ -73,4 +61,85 @@ var piman_edit = function(entity_type, id) {
   .error(on_error)
 }
 
+function piman_clear_overlay(){
+  $("#overlay").html('').hide();
+}
+
+function piman_fill_show(div, url){
+  div = '#' + div;
+  var on_success = function(data, textStatus, XMLHttpRequest){
+                      try{
+                         if(data){
+                           if(check_authentication(data)){
+                             $(div).html(data.html).show();
+                           }else{
+                             alert("not logged in");
+                           }
+                         }
+                       }catch(error){
+                         $("#header").append("<h1>Error parsing JSON</h1>");
+                       }
+
+  }
+
+  var on_error = function(XMLHttpRequest, textStatus, errorThrown){
+                    piman_send_message(errorThrown, true);
+                    // $("#header").append("<h1 class='error'>Error: " + errorThrown + "</h1>");
+                    
+                    }
+
+   $.ajax({
+    url: url,
+    data: {},
+  })
+  .success(on_success)
+  .error(on_error)
+}
+
+function piman_clear_hide(div){
+  div = '#'+div;
+  $(div).html('').hide();
+}
+
+
+
+
+var piman_register = function(){
+  url = '/accounts/register';
+  piman_overlay(url);
+}
+
+var piman_edit = function(entity_type, id) {
+
+  url =  '/' + entity_type + '/' + id + '/' + 'edit';
+  piman_fill_show('overlay',url);
+
+}
+
+var piman_create = function(entity_type, id){
+  url = '/' + entity_type + '/' + 'create';
+  piman_overlay(url);
+}
+
+var piman_submit = function(formid, url, success, error){
+
+  formid = '#' + formid;
+  id = $(formid+" > .id").html();
+
+  if (!success){  
+      success = function(data, txt, xhr){piman_send_message("Form successfully submitted " + data.html)};      
+  }
+  if(!error){
+      error = function(xhr, txt, err){piman_send_message("Form submission failed. " + txt, true)};
+  }
+
+  $.ajax({type:'POST', 
+          url: url,  
+          data:$(formid).serialize(), 
+          success: success,
+          error: error
+         });
+  
+  return(true);
+}
 
